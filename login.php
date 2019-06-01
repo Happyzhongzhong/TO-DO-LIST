5106ff81a3d0b6dcde2448aefa5f4079f52ec42a
happyzhonghzhong <532583058@qq.com>
2019-06-01 20:00:52 +0800

    hello

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
 <?php
+//  启动 Session
+session_start();
+//  声明一个名为 admin 的变量，并赋空值。
+$_SESSION["admin"] = null;
+?>
+
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -14,11 +21,14 @@ try {
         echo "用户名和密码不能为空，请返回重新登陆";
     }
     else{ 
+        $posts = $_POST;
         $stmt = $conn->prepare("SELECT * FROM user 
         WHERE username = '$username' and passwordbase = '$password'"); 
         $stmt->execute(); 
         $num = count($stmt->fetchAll());
         if($num){
+            //  注册登陆成功的 admin 变量，并赋值 true
+            $_SESSION["admin"] = true;
             echo "欢迎回来，每一天都是新的开始。";
         }
         else{
